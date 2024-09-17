e37402515895cd066dcb888eb811ed3e4729929d
MartinXCVI <mdzg96@gmail.com>
2024-09-17 00:08:49 -0300

    small fixes in mobile navbar menu

--- src/scripts/hamMenu.js
+++ src/scripts/hamMenu.js
@@ -1,6 +1,7 @@
 const hamIcon = document.querySelector('.ham-icon')
 const closeIcon = document.querySelector('.close-icon')
 const navMenu = document.querySelector('.header-element .navbar')
+const body = document.getElementById('body')
 
 
 hamIcon.addEventListener('click', ()=> {
@@ -13,12 +14,16 @@ closeIcon.addEventListener('click', ()=> {
 
 function displayMenu() {
   navMenu.style.minHeight = '105vh'
+  navMenu.style.height = '105vh'
   navMenu.style.overflow = 'unset'
   closeIcon.style.opacity = '0.9'
+  body.style.overflow = 'hidden'
 }
 
 function hideMenu() {
   navMenu.style.minHeight = '0vh'
+  navMenu.style.height = '0vh'
   navMenu.style.overflow = 'hidden'
   closeIcon.style.opacity = '0'
+  body.style.overflow = 'unset'
 }
