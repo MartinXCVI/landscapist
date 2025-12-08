23aa727cf8fbe795a9490595af96470b2be38fec
MartinXCVI <mdzg96@gmail.com>
2025-12-08 01:15:17 -0300

    Added sitemap and robots.txt settings.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap'
+import { siteConfig } from 'src/config/site.config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: siteConfig.site.url,
+  integrations: [
+    sitemap({
+      // Excluded pages from sitemap
+      filter: (page) => !page.includes('/404'),
+      // any additional pages not in src/pages/
+      customPages: [],
+    })
+  ],
+});
